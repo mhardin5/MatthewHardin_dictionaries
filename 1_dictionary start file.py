0b7205be364e6d9e3d038219adceed472f50078b
mhardin5 <matthew_hardin1@baylor.edu>
2025-02-24 01:59:48 -0600

    Initial commit

---
+++ 1_dictionary start file.py
@@ -0,0 +1,158 @@
+import random
+
+phonebook = {'Chris':'555−1111',
+             'Katie':'555−2222',
+             'Joanne':'555−3333'}
+
+
+
+print()
+print('*****  start section 1 - print dictionary ********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 1 ********')
+print()
+
+
+'''
+
+
+print()
+print('*****  start section 2 - search dictionary ********')
+print()
+
+
+
+
+
+
+
+print()
+print('*****  end section 2 ********')
+print()
+
+
+
+
+
+
+
+print()
+print('*****  start section 3 - edit/append dictionary ********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 3 ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  start section 4 - delete/remove from dictionary ********')
+print()
+
+
+
+
+print()
+print('*****  end section 4 ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  start section 5 - iterate through keys, values, items ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  end section 5 ********')
+print()
+
+
+
+
+
+print()
+print('*****  start section 6 - using get and clear ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  end section 6 ********')
+print()
+
+
+
+print()
+print('*****  start section 7 - using pop method ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  end section 7 ********')
+print()
+
+
+
+print()
+print('*****  start section 8 - using popitem ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  end section 8 ********')
+print()
+
+
+
+print()
+print('*****  start section 9 - using random and converting to list ********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 9 ********')
+print()
+
+
+'''
+
+
+
+
+
